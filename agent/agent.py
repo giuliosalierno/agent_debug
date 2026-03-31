41415cc56853358336e63fd01bbd5d6096918bdb
Giulio Salierno <giuliosalierno@google.com>
2026-03-31 21:54:15 +0000

    Initial commit

---
+++ agent/agent.py
@@ -0,0 +1,33 @@
+import os
+
+import google.auth
+from dotenv import load_dotenv
+from google.adk.agents import Agent
+from google.adk.apps.app import App
+
+from .prompts import MOVA_INSTRUCTION
+from .tools import animate_frame, create_frame
+
+# Load environment variables from .env file
+load_dotenv()
+
+_, project_id = google.auth.default()
+os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
+os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
+os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
+
+# --- Root Agent ---
+root_agent = Agent(
+    name="Mova",
+    model="gemini-2.5-flash",
+    instruction=MOVA_INSTRUCTION,
+    description="This is the root agent that orchestrates the video generation process.",
+    tools=[create_frame, animate_frame],
+)
+
+app_name = os.environ.get("GOOGLE_CLOUD_AGENT_ENGINE_ID", "agent")
+
+app = App(
+    name=app_name,
+    root_agent=root_agent
+)
